0ba0245ea39204ba2df1df1b082365340ab499f9
Ali Zulfikar <alizulfikar032@gmail.com>
2022-07-15 21:01:02 +0700

    Learn: Vue Component

---
+++ install-cli/src/components/Logos.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>Find Best Logo only at Logoos!</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Logos',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
